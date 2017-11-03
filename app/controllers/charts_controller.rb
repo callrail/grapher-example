e5074c0c506aaa0e7c9641899efc68c07f4113f7
tallkeith <keithpnash@gmail.com>
2017-11-03 15:49:00 -0400

    replaced account and company ids with env vars

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -8,7 +8,7 @@ class ChartsController < ApplicationController
   end
 
   def calls_by_date
-    calls = summary("https://api.callrail.com/v2/a/170499692/calls/timeseries.json?company_id=385497224&start_date=2017-06-14&end_date=2017-09-14&fields=total_calls,missed_calls")
+    calls = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/timeseries.json?company_id=#{ENV['COM_ID']}&date_range=recent&fields=total_calls,missed_calls")
     data = calls['data']
     total_calls = data.map { |c| [c["date"], c["total_calls"]] }
     missed_calls = data.map { |c| [c["date"], c["missed_calls"]] }
@@ -19,18 +19,27 @@ class ChartsController < ApplicationController
   end
 
   def first_vs_repeat
-    callers = summary("https://api.callrail.com/v2/a/170499692/calls/summary.json?company_id=385497224&start_date=2017-06-14&end_date=2017-09-14&group_by=source&fields=first_time_callers")
-    results = callers['grouped_results']
+    callers = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}&start_date=2017-06-14&end_date=2017-09-14&group_by=source&fields=first_time_callers")
+    results = callers['grouped_results'].reject { |c| c["key"] == "Outbound Call" || c["key"].nil? }
     first_timers = results.map { |c| [c["key"], c["first_time_callers"]] }
     repeat_callers = results.map { |c| [c["key"], c["total_calls"] - c["first_time_callers"]]}
     bar_chart_data = [
-      {name: "First Time Callers", data: first_timers},
-      {name: "Repeat Callers", data: repeat_callers}
+      {name: "Repeat Callers", data: top_five(repeat_callers)},
+      {name: "First Time Callers", data: top_five(first_timers)}
     ]
   end
 
+# how do I handle cases where the top 5 is different for first time vs repeat callers?
+  def top_five(mapped)
+    five_arr = []
+    high_five = mapped.sort {|a,b| b[1] <=> a[1]}
+    high_five[0..4].each do |kw|
+      five_arr << kw
+    end
+  end
+
   def generate_pie_chart
-    keywords = summary("https://api.callrail.com/v2/a/266101466/calls/summary.json?company_id=297407543&start_date=2017-06-14&end_date=2017-09-14&group_by=keywords")
+    keywords = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}&start_date=2017-06-14&end_date=2017-09-14&group_by=keywords")
     # create array of arrays of Keywords and associated number of calls
     unsorted = keywords['grouped_results']
     sorter(unsorted)
@@ -42,8 +51,7 @@ class ChartsController < ApplicationController
     kws = kw_array.map { |c| [c['key'], c['total_calls']] }
     # Sort array of arrays by number of calls
     sorted = kws.sort {|a,b| b[1] <=> a[1]}
-    # report the top 4 sources and combine the rest into "other" array,
-    # excluding calls with "nil" keywords
+    # report the top 4 sources and combine the rest into "other" array
     other = []
     sorted_data = []
     sorted[0..3].each do |kw|

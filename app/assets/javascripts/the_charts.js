477fb52587ee06910a5e3b67df9a104748955977
tallkeith <keithpnash@gmail.com>
2017-11-15 13:15:16 -0500

    dropdown boxes now reload page with correct query

--- app/assets/javascripts/the_charts.js
+++ app/assets/javascripts/the_charts.js
@@ -1,28 +1,5 @@
-function tags() {
-  var dropdown = $("#tag_id");
-  dropdown.change(function() {
-    $.getJSON("/charts/tag_data?tag=" + dropdown[0].value, function(data) {
-      new Chartkick.PieChart("chart-3", data, {"donut":true});
-    });
-  });
-};
-
-function date_range() {
-  var dropdown = $("#date_range");
-  dropdown.change(function() {
-    $.getJSON("/charts/calls_by_date?date_range=" + dropdown[0].value, function(data) {
-      new Chartkick.AreaChart("chart-1", data);
-    });
-    $.getJSON("/charts/first_vs_repeat?date_range=" + dropdown[0].value, function(data) {
-      new Chartkick.BarChart("chart-2", data, {stacked: true});
-    });
-    $.getJSON("/charts/generate_pie_chart?date_range=" + dropdown[0].value, function(data) {
-      new Chartkick.PieChart("chart-3", data, {donut: true});
-    });
-  });
-};
-
-$(function() {
-    date_range();
-    tags();
-});
+function refetchPage() {
+  var tag = $("#tag_id").val();
+  var range = $("#date_range").val();
+  location.href = "/charts?tag=" + tag + "&date_range=" + range;
+}
--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,9 +2,20 @@ class ChartsController < ApplicationController
   # GET /charts
   # GET /charts.json
   def index
+    @params = params
     @line_chart_data = calls_by_date_data
     @bar_chart_data = first_vs_repeat_data
     @pie_chart_kws_data = generate_pie_chart_data
+  end
+
+  def sorter(kw_array)
+    kw_array = kw_array.reject { |c| c["key"].nil? }
+    kws = kw_array.map { |c| [c['key'], c['total_calls']] }
+    sorted = kws.sort {|a,b| b[1] <=> a[1]}
+    sorted_data = sorted.take(4)
+    other = sorted.drop(4)
+    other_arr = ["other", other.sum { |o| o[1] }]
+    sorted_data << other_arr
   end
 
   def calls_by_date
@@ -13,7 +24,8 @@ class ChartsController < ApplicationController
 
   def calls_by_date_data
     date_range = params[:date_range]
-    calls = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/timeseries.json?company_id=#{ENV['COM_ID']}&fields=total_calls,missed_calls&date_range=#{date_range}")
+    tag = params[:tag]
+    calls = api_request(:timeseries, {fields: 'total_calls,missed_calls'})
     data = calls['data']
     total_calls = data.map { |c| [c["date"], c["total_calls"]] }
     missed_calls = data.map { |c| [c["date"], c["missed_calls"]] }
@@ -29,7 +41,10 @@ class ChartsController < ApplicationController
 
   def first_vs_repeat_data
     date_range = params[:date_range]
-    callers = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}&date_range=#{date_range}&group_by=source&fields=first_time_callers")
+    tag = params[:tag]
+    callers = api_request(:summary, {group_by: 'source', fields: 'first_time_callers'})
+
+    # summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}&date_range=#{date_range}&group_by=source&fields=first_time_callers")
     results = callers['grouped_results'].reject { |c| c["key"] == "Outbound Call" || c["key"].nil? }
     results = top_five(results)
     first_timers = results.map { |c| [c["key"], c["first_time_callers"]] }
@@ -40,7 +55,6 @@ class ChartsController < ApplicationController
     ]
   end
 
-# how do I handle cases where the top 5 is different for first time vs repeat callers?
   def top_five(mapped)
     high_five = mapped.sort {|a,b| b['total_calls'] <=> a['total_calls']}
     high_five.take(5)
@@ -52,59 +66,59 @@ class ChartsController < ApplicationController
 
   def generate_pie_chart_data
     date_range = params[:date_range]
-    keywords = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}&date_range=#{date_range}&group_by=keywords")
+    keywords = api_request(:summary, {group_by: 'keywords'})
     # create array of arrays of Keywords and associated number of calls
     unsorted = keywords['grouped_results']
     sorter(unsorted)
   end
 
-  def sorter(kw_array)
-    # Sanitize response by removing "nil" keyword from the results
-    kw_array = kw_array.reject { |c| c["key"].nil? }
-    kws = kw_array.map { |c| [c['key'], c['total_calls']] }
-    # Sort array of arrays by number of calls
-    sorted = kws.sort {|a,b| b[1] <=> a[1]}
-    # report the top 4 sources and combine the rest into "other" array
-    other = []
-    sorted_data = []
-    sorted[0..3].each do |kw|
-      sorted_data << kw
-    end
-    sorted[4..-1].each do |kw|
-      other << kw[1][1]
-    end
-    other_arr = ["other", other.sum]
-    sorted_data << other_arr
-  end
-
   def tag_data
     tag = params[:tag]
-    tags = summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}&start_date=2017-06-14&end_date=2017-09-14&group_by=keywords&tags[]=#{tag}")
+    tags = api_request(:summary, {group_by: 'keywords', tags: [tag]})
+    # summary("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}}&date_range=#{date_range}&group_by=keywords&tags[]=#{tag}")
     results = tags['grouped_results']
     render json: sorter(results)
   end
 
-  # def dater
-  #   case date_range
-  #     when 'recent'
-  #       date_range = 'recent'
-  #     when 'today'
-  #        date_range = 'today'
-  #     when 'yesterday'
-  #       date_range = 'yesterday'
-  #     when 'last 7 days'
-  #       date_range = 'last_7_days'
-  #     when 'last 30 days'
-  #       date_range = 'last_30_days'
-  #     when 'this month'
-  #       date_range = 'this_month'
-  #     when 'last month'
-  #       date_range = 'last_month'
-  #   end
-
 private
 
-  def summary(url)
-    JSON.parse(RestClient.get(url, {Authorization: "Token token=#{ENV['CR_API_KEY']}"}))
+  # endpoint: "summary" or "timeseries"
+  # query: hash of URL params for this request
+  def api_request(endpoint, query)
+    query[:company_id] = ENV['COM_ID']
+    query[:date_range] = params[:date_range]
+    query[:tags] = params[:tag]
+
+    JSON.parse(
+      RestClient.get(
+        "https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/#{endpoint}.json",
+        params: query,
+        Authorization: "Token token=#{ENV['CR_API_KEY']}"
+      )
+    )
   end
+
+
+  # needs to be able to change from "timeseries" to "summary"
+  def summary_api_route
+    # date_range = params[:date_range]
+    # JSON.parse(RestClient.get("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/summary.json?company_id=#{ENV['COM_ID']}}&date_range=#{date_range}&group_by=keywords", {Authorization: "Token token=#{ENV['CR_API_KEY']}"}))
+    api_request(:summary, {group_by: 'keywords'})
+  end
+
+  def timeseries_api_route
+    # JSON.parse(RestClient.get("https://api.callrail.com/v2/a/#{ENV['ACCT_ID']}/calls/timeseries.json?company_id=#{ENV['COM_ID']}&fields=total_calls,missed_calls&date_range=#{date_range}", {Authorization: "Token token=#{ENV['CR_API_KEY']}"}))
+    api_request(:timeseries, {fields: 'total_calls,missed_calls', group_by: 'something'})
+  end
+
+  # create 3 methods that handles the API endpoint stuff.
+  # summary stuff
+  # time stuff
+  # HTTP heavy lifting
+
+
+  # "summary" needs to provide grouping options and field options
+  # needs to be able to omit or include "tag" url info
+
+
 end

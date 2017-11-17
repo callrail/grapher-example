92043709de91a021fc07c03b93ee73ef7f47042c
tallkeith <keithpnash@gmail.com>
2017-11-17 16:58:10 -0500

    added comments and removed supurfluous routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :charts, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'charts#index'
 
 
 end

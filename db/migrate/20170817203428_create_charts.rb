17e9fe386e158e15b59664aaad49bda412ef2fb5
tallkeith <keithpnash@gmail.com>
2018-01-19 11:38:06 -0500

    Removed several unused boilerplate files

--- db/migrate/20170817203428_create_charts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCharts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :charts do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
--- test/system/charts_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class ChartsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit charts_url
-  #
-  #   assert_selector "h1", text: "Chart"
-  # end
-end
